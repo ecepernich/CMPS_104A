76ebb6bc63fc69a912d411f5ae8514c172140bca
ecepernich <ecepernich@gmail.com>
2016-10-24 21:36:14 -0700

    formatting

--- lyutils.cpp
+++ lyutils.cpp
@@ -46,7 +46,7 @@ void lexer::newline() {
 int yylval_token(int symbol)
 {
    astree* ast1=new astree(symbol, lexer::lloc, yytext);
-   fprintf(tokfile, "%zu \t %zu.%zu \t %i \t %.*s \t (%.*s) \n",
+   fprintf(tokfile, "%zu \t %zu.%zu \t %i \t %s \t \t (%s) \n",
            lexer::lloc.filenr,
            lexer::lloc.linenr,
            lexer::lloc.offset,
