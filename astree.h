55290951fc8cb0fc30a615a062121c2c80fef212
ecepernich <ecepernich@gmail.com>
2016-11-07 12:23:00 -0800

    astree dump

--- astree.h
+++ astree.h
@@ -35,6 +35,7 @@ struct astree {
    void dump_node (FILE*);
    void dump_tree (FILE*, int depth = 0);
    static void dump (FILE* outfile, astree* tree);
+   static void dump2 (FILE* outfile, astree* tree);
    static void print (FILE* outfile, astree* tree, int depth = 0);
    astree* convert (int symbol_);
 };
