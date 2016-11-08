52669155c0e035a0bbaadf9bb908fab48339faf9
ecepernich <ecepernich@gmail.com>
2016-11-07 18:51:44 -0800

    cleaning up

--- astree.cpp
+++ astree.cpp
@@ -74,7 +74,7 @@ void astree::dump (FILE* outfile, astree* tree) {
 
 void astree::print (FILE* outfile, astree* tree, int depth) {
    //fprintf (outfile, "|   %*s", depth * 2, "");
-   for (int i=0;i<depth+1;i++)
+   for (int i=0;i<depth;i++)
    {
       fprintf(outfile, "|   ");
    } 
