b1458aa603bbceac2fc66fb9df8f0e5d55652d80
ecepernich <ecepernich@gmail.com>
2016-11-15 22:19:03 -0800

    coming up with switch list

--- typecheck.cpp
+++ typecheck.cpp
@@ -4,3 +4,69 @@
 // Assignment 4: .sym file
 
 //implementing figure 1 grammar
+
+
+
+
+//upcoming switch statement for something
+
+	switch(node->symbol)
+	{
+		case TOK_ROOT:
+
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+		case '[':
+		case ']':
+		case ';':
+
+		case TOK_RETURN:
+		case TOK_PARAM:
+
+
+		case TOK_NEW:
+		case TOK_TYPEID:
+		case TOK_DECLID:
+		case TOK_FIELD: 
+		case TOK_FUNCTION:
+		case TOK_PROTOTYPE: 
+
+		case TOK_CALL:
+		case TOK_CHAR:
+		case TOK_INT:
+		case TOK_BOOL
+		case TOK_VOID:
+		case TOK_STRING:
+		case TOK_ARRAY:
+		case TOK_NEWARRAY: 
+		case TOK_VOID:
+		case TOK_INDEX: 
+		case TOK_IDENT:
+		case TOK_STRUCT:
+		case TOK_IF:
+		case TOK_IFELSE:
+		case TOK_WHILE:
+
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '%': 
+		case '!':
+		case '=':
+		case TOK_EQ:
+		case TOK_GT:
+		case TOK_GE:
+		case TOK_LT:
+		case TOK_LE:
+
+		case TOK_INTCON:
+		case TOK_CHARCON:
+		case TOK_STRINGCON:
+		case TOK_NULL:
+
+			break;
+	}
+	
