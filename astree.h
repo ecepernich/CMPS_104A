abcde9822e2651e9f6bdcf22220387863202ef2e
ecepernich <ecepernich@gmail.com>
2016-11-21 14:43:33 -0800

    starting to get rid of errors

--- astree.h
+++ astree.h
@@ -14,8 +14,8 @@ using namespace std;
 
 //move to astree.h
 enum {  ATTR_void, ATTR_int, ATTR_null, ATTR_string, ATTR_struct, ATTR_array, 
-        ATTR_function, ATTR_variable, ATTR_field, ATTR_typeid, ATTR_param, ATTR_lval
-        ATTR_vreg, ATTR_vaddr, ATTR_bitset_size,
+        ATTR_function, ATTR_variable, ATTR_field, ATTR_typeid, ATTR_param, ATTR_lval,
+        ATTR_vreg, ATTR_vaddr, ATTR_bitset_size
 };
 using attr_bitset = bitset<ATTR_bitset_size>;
 
