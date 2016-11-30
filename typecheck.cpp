2db677bd734c2801a60e73af95f3cd33001f4afd
ecepernich <ecepernich@gmail.com>
2016-11-29 16:38:49 -0800

    typecheck debugs

--- typecheck.cpp
+++ typecheck.cpp
@@ -270,6 +270,7 @@ void typecheck_function(FILE* symfile, astree* node,
         }
         case TOK_STRUCT: {
             current_struct=node;
+            current_field=node;
 
             node->attr[ATTR_struct]=1;
             left->attr[ATTR_struct]=1;
