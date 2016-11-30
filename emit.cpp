c98f9fa15f3ed8b2a5452a016bed0717e215d2af
ecepernich <ecepernich@gmail.com>
2016-11-30 14:19:29 -0800

    more emitting

--- emit.cpp
+++ emit.cpp
@@ -1,9 +1,12 @@
 #include "emit.h"
 
+int strincon_nr=0;
+
 void emit_everything(oilfile, astree* root)
 {
 	emit_header(oilfile);
-	emit_program(oilfile);
+	emit_program(oilfile, root);
+	emit_main(oilfile, root);
 
 }
 
@@ -23,17 +26,29 @@ void emit_stringdef(FILE* oilfile)
 
 }
 
-void emit_function(FILE* oilfile)
+void emit_function(FILE* oilfile, astre* node)
 {
-
+	if (node->children.size()>=1)
+	{
+		emit_function_name(oilfile, node);
+	}
+	if (node->children.size()>=2)
+	{
+		emit_params(oilfile, node);
+	}
+	if (node->children.size()>=3)
+	{
+		emit_function_body(oilfile, node);
+	}
 }
 
-void emit_function_name(astree* node, FILE* oilfile){
-	astree* left = node->children[0];
-	fprintf(oilfile, "__%s", lexinfo-c_str());
+void emit_function_name(FILE* oilfile, astree* node)
+{
+	astree* left=node->children[0];
+	fprintf(oilfile, "__%s ", lexinfo->c_str());
 }
 
-void emit_stringcon(FILE* oilfile)
+void emit_stringcon(FILE* oilfile, astree* node)
 {
 	//expected result:
 	//char* s1 = "Hello World!\n"
@@ -46,27 +61,41 @@ void emit_stringcon(FILE* oilfile)
 
 }
 
-void emit_params(astree* node, FILE* oilfile){
+void emit_params(astree* node, FILE* oilfile)
+{
+	fprintf(oilfile, " (");
 	astree* paramhead = nullptr;
-	if(node->children.size() >= 2){
+	if(node->children.size()>=2)
+	{
 		paramhead = node->children[1];
-		fprintf(oil, "%s",paramhead->emit_code );
-		int i = 0;
-		astree* plist = nullptr;
-		if (paramhead->children.size()>=1){
-			while(plist != nullptr)
-				fprintf(oil, "%s",plist->emit_code);
-			    //maybe switch 
-			     i++
-		}
-		if (paramhead->children.size()>+ i+1)
+		fprintf(oilfile, "%s",paramhead->emit_code);
+		int i=0;
+		astree* plist=nullptr;
+		if (paramhead->children.size()>=1)
 		{
-			plist = paramhead->children[i];
-		}else{
-			plist= nullptr;
+			while(plist!=nullptr)
+			{
+				fprintf(oilfile, "%s",plist->emit_code);
+			    //maybe switch 
+			    i++;
+				if (paramhead->children.size()>=(i+1))
+				{
+					plist=paramhead->children[i];
+				}
+				else
+				{
+
+					plist=nullptr;
+				}
+			}
 		}
 	}
+	fprintf(oilfile, ") ");
+}
 
+void emit_function_body(FILE* oilfile, astree* node)
+{
+	//
 }
 
 void emit(astree* node,FILE* oilfile)
@@ -79,6 +108,7 @@ void emit(astree* node,FILE* oilfile)
 		     {emit_stringcon(oilfile, node)
 		      break;
 		      }
+		case TOK_FUNCTION:
 		case TOK_CALL:
             //expected result:__puts(s1)
 
