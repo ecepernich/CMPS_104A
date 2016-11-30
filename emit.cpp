a59767dae433996ff1620ff98c7c819161d1b8d3
ecepernich <ecepernich@gmail.com>
2016-11-29 23:29:51 -0800

    emitting

--- emit.cpp
+++ emit.cpp
@@ -3,36 +3,37 @@
 void emit_everything(oilfile, astree* root)
 {
 	emit_header(oilfile);
-	
+	emit_program(oilfile);
+
 }
 
-void emit_header(oilfile)
+void emit_header(FILE* oilfile)
 {
 	fprintf(oilfile,"define __OCLIB_C__\n");
 	fprintf(oilfile, "#include \"oclib.oh\"\n");
 }
 
-void emit_program()
+void emit_program(FILE* oilfile)
 {
 
 }
 
-void emit_stringdef()
+void emit_stringdef(FILE* oilfile)
 {
 
 }
 
-void emit_function()
+void emit_function(FILE* oilfile)
 {
 
 }
 
-void emit_stringcon()
+void emit_stringcon(FILE* oilfile)
 {
 
 }
 
-void emit()
+void emit(FILE* oilfile)
 {
 	
 }
