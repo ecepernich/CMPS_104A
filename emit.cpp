2a9e19704d661b99aeb5cc754ff621532e0547c2
ecepernich <ecepernich@gmail.com>
2016-11-29 17:14:52 -0800

    emit files

---
+++ emit.cpp
@@ -0,0 +1,36 @@
+#include "emit.h"
+
+void emit_everything()
+{
+
+}
+
+void emit_header()
+{
+
+}
+
+void emit_program()
+{
+
+}
+
+void emit_stringdef()
+{
+
+}
+
+void emit_function()
+{
+
+}
+
+void emit_stringcon()
+{
+
+}
+
+void emit()
+{
+	
+}
---
+++ emit.h
@@ -0,0 +1,7 @@
+#include "astree.h"
+#include "symstack.h"
+#include "lyutils.h"
+#include "auxlib.h"
+#include <string>
+#include <iostream>
+
