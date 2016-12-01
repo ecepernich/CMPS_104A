177314a8a80c2f53fd9c91ad64f9cb832d0e3eae
ecepernich <ecepernich@gmail.com>
2016-11-30 23:03:18 -0800

    please bitbucket

--- emit.cpp
+++ emit.cpp
@@ -35,8 +35,8 @@ void emit_structdecl(FILE* oilfile, astree* node)
         {
             left=node->children[0];
             fprintf(oilfile, "struct s_%s {\n", left->lexinfo->c_str());
-            std::string str="s_";
-            str.append(left->lexinfo->c_str());
+            std::string structname="s_";
+            structname.append(left->lexinfo->c_str());
             node->emit_code=structname->c_str();
             if (node->children.size()>=2)
             {
