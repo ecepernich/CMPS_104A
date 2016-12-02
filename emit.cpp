7c8ccce97e8da04d1992a30235c3c28ae0a2c230
ecepernich <ecepernich@gmail.com>
2016-12-01 23:07:27 -0800

    hhhhhh

--- emit.cpp
+++ emit.cpp
@@ -152,7 +152,7 @@ void emit_vardecl(FILE* oilfile, astree* node)
     {
         astree* left=nullptr;
         astree* left2=nullptr;
-        const char* varname = "__";
+        char* varname = "__";
 
         if (node->children.size()>=1)
         {
